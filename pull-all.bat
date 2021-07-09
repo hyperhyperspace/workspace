d0a9f4e1ca54c4899d0d4d2640762f483050479a
Santi Bazerque <sbazerque@gmail.com>
2021-07-09 17:00:44 -0300

    added a script to pull all dependencies on windows

---
+++ pull-all.bat
@@ -0,0 +1,1 @@
+cd "@hyper-hyper-space/node-env" && git pull && cd ..  && cd core && git pull && cd .. && cd react && git pull && cd .. && cd p2p-chat && git pull && cd .. && cd p2p-chat-cli && git pull && cd .. && cd p2p-chat-web && git pull && cd .. && cd soliton && git pull && cd .. && cd ../..
