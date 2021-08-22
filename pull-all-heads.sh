440d4f06fbe300fee2069556cb6ebec1b930c05b
Santi Bazerque <sbazerque@gmail.com>
2021-08-22 20:13:02 -0300

    update: when pulling everything, switch to the main branch before (as submodules are usually initialized with a detached head)

---
+++ pull-all-heads.sh
@@ -0,0 +1,29 @@
+cd "@hyper-hyper-space/node-env"
+git checkout master
+git pull
+cd .. 
+cd core
+git checkout master
+git pull
+cd ..
+cd react
+git checkout master
+git pull
+cd ..
+cd p2p-chat
+git checkout master
+git pull
+cd ..
+cd p2p-chat-cli
+git checkout master
+git pull
+cd ..
+cd p2p-chat-web
+git checkout master
+git pull
+cd ..
+cd soliton
+git checkout main
+git pull
+cd ..
+cd ../..
